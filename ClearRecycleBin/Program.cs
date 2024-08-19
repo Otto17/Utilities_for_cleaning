0e46e270f630577e79514a6d27e864c7286b3804
Otto <den-life@mail.ru>
2024-08-19 18:20:11 +0600

    19.08.24

---
+++ ClearRecycleBin/Program.cs
@@ -0,0 +1,58 @@
+﻿/*
+   Простая программа, которая при запуске автоматически очищает корзину на всех дисках, не требуя подтверждения от пользователя.
+
+   Делал для личного использования под свои нужды.
+
+   Данная программа является свободным программным обеспечением, распространяющимся по лицензии MIT.
+   Копия лицензии: https://opensource.org/licenses/MIT
+   Copyright (c) 2024 Otto
+   Автор: Otto
+   Версия: 19.08.24
+   GitHub страница:  https://github.com/Otto17/Utilities_for_cleaning
+   GitFlic страница: https://gitflic.ru/project/otto/utilities_for_cleaning
+   г. Омск 2024
+*/
+
+
+using System;                           // Библиотека предоставляет доступ к базовым классам и функциональности .NET Framework
+using System.Runtime.InteropServices;   // Библиотека предоставляет классы для работы с взаимодействием между управляемым и неуправляемым кодом, включая работу с COM-объектами и вызовами нативного кода
+
+namespace ClearRecycleBin
+{
+    class Program
+    {
+        //Импортируем функцию "SHEmptyRecycleBin" из "shell32.dll"
+        [DllImport("shell32.dll", SetLastError = true)]
+
+        static extern uint SHEmptyRecycleBin(IntPtr hwnd, string pszRootPath, uint dwFlags);    // Используется для очистки корзины
+
+        //Флаги для "SHEmptyRecycleBin"
+        const uint SHERB_NOCONFIRMATION = 0x00000001;   // Отключает подтверждающие диалоги
+        const uint SHERB_NOPROGRESSUI = 0x00000002;     // Отключает отображение диалога прогресса
+        const uint SHERB_NOSOUND = 0x00000004;          // Отключает звуковое сопровождение
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Очистка корзины...");
+
+                //Очищаем корзину на всех дисках
+                uint result = SHEmptyRecycleBin(IntPtr.Zero, null, SHERB_NOCONFIRMATION | SHERB_NOPROGRESSUI | SHERB_NOSOUND);
+
+                if (result == 0)
+                {
+                    Console.WriteLine("Корзина успешно очищена.");
+                }
+                else
+                {
+                    Console.WriteLine($"Произошла ошибка при очистке корзины. Код ошибки: {result}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ ClearRecycleBin/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ClearRecycleBin")]
+[assembly: AssemblyDescription("Простая программа, которая при запуске автоматически очищает корзину на всех дисках, не требуя подтверждения от пользователя")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ClearRecycleBin")]
+[assembly: AssemblyCopyright("Copyright ©  2024 Otto")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("d7b8747e-3282-41c9-914f-914213bd8f1e")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("19.08.24")]
+[assembly: AssemblyFileVersion("19.08.24")]
